285affbc5ae98705713ff1a827fb0f8ecaa92852
luigimarino <146132465+luigimarino@users.noreply.github.com>
2025-03-19 09:06:38 +0000

    Update record.sh

--- record.sh
+++ record.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/common.sh
+script_name=$(basename "${BASH_SOURCE[0]}")
 
 # You can record multiple files simultaneously: if the pi is already recording this will still work, so no need to code for an exit. 
 # Anyways, I created a recording status variable, so I can still retrieve the info.
@@ -11,12 +12,12 @@ record_file="$AUDIO_FOLDER/$record_timestamp.wav"
 
 #log "\n"
 log "______________RECORDING"
-log "record.sh - Start recording"
+log "$script_name - Start recording"
 sudo arecord -d $RECORD_DURATION -D mic_out_shared -c 2 -r 44100 -f S16_LE -t wav -V stereo -v $record_file
 if [ -f $record_file ]; then
-    log "record.sh - Recorded $record_timestamp.wav"
+    log "$script_name - Recorded $record_timestamp.wav"
 else
-    log "record.sh - Recording failed" 
+    log "$script_name - Recording failed" 
 fi
 
 set_record_state 0
