c3fb6850f0d8a320d7cafa5d7c445112eb8394ee
luigimarino <146132465+luigimarino@users.noreply.github.com>
2025-03-19 09:06:19 +0000

    Update nuclear-option.sh

--- nuclear-option.sh
+++ nuclear-option.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/common.sh
+script_name=$(basename "${BASH_SOURCE[0]}")
 
 # This check is separate from monitor.sh because reboot also sends the log email, and too many emails are annoying...
 # With crontab you can decide how often to call this last resource.
 if [[ ! status_darkice || $(get_darkice_state) -eq 0 ]]; then 
-	log "nuclear-option.sh - DARKICE IS UP TO NO GOOD -> SELF DESTRUCTION (no worries, just a reboot...)"
+	log "$script_name - DARKICE IS UP TO NO GOOD -> SELF DESTRUCTION (no worries, just a reboot...)"
 	sudo reboot
 fi
 
@@ -13,7 +14,7 @@ fi
 if [[ -n $DONGLE && $STREAM_MODE -ne 1 ]]; then
 	# Check if the usb dongle is detected. 
 	if ! ip addr show usb0; then
-		log "nuclear-option.sh - ERROR: No usb0 device found -> REBOOT THE RPI!"
+		log "$script_name - ERROR: No usb0 device found -> REBOOT THE RPI!"
 		sudo reboot
 	fi
 fi
