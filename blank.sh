8dcae1878f6458c6c359d8e5fb129135244abb12
luigimarino <146132465+luigimarino@users.noreply.github.com>
2025-04-14 17:28:33 +0100

    Update blank.sh
    Use 'source' instead of 'bash' command to inherit variables from parent to child script and vice-versa.

--- blank.sh
+++ blank.sh
@@ -14,15 +14,19 @@ source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/common.sh
 script_name=$(basename "${BASH_SOURCE[0]}")
 
 commands_script="$PROJECT_FOLDER/commands-tray.sh"
+# Create temp log to capture errors from the sourced commands script
+temp_log=$(mktemp)
 
 # Check if commands-tray.sh exists and is not empty and execute its commands
 if [[ -f "$commands_script" && -s "$commands_script" ]]; then
     log "$script_name - Executing $commands_script..."
     log "$(cat $commands_script)"
-    # Note bash instead of source: no inheritance from parent shell but it logs errors (stderr)
-    bash "$commands_script" 2>&1 | tee -a "$LOG_FILE"
+    # Run the script in current shell (source and not bash), capture both stdout and stderr
+    source "$commands_script" > >(tee -a "$temp_log") 2>&1
+    # Log the full output
+    log "$(cat "$temp_log")"
     log "$script_name - Execution completed. Deleting $commands_script..."
-    sudo rm -f "$commands_script"
+    sudo rm -f "$commands_script" && rm -f "$temp_log"
 else
     log "$script_name - No commands to execute."
 fi
@@ -40,33 +44,40 @@ fi
 
 # You can´t use ´sudo reboot´ normally to reboot in commands-tray.sh or these script won´t finish.
 # To reboot the RPi with commands-tray.sh, write as last command:  
-# reboot=1
-
-# commands-tray.sh is executed with the bash command so think of it like typing on a new terminal session:
-# if you need the global variables from common.sh, you'll need to source again the script.
-# source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/common.sh
-# echo $PROJECT_FOLDER
+## reboot=1
 
 # Change mic volume
-# amixer set MicBoost 60%
+## amixer set MicBoost 60%
 
 # Add a new cronjob remotely:
-# crontab -l | { cat; echo "* * * * * echo 'ahahah' >> test.txt"; } | crontab -
+## crontab -l | { cat; echo "* * * * * echo 'ahahah' >> test.txt"; } | crontab -
+
+# commands-tray.sh is executed with source so it inherits all the variables and functions from the prent script.
+# The var PROJECT_FOLDER and the log function are defined in common.sh 
+## echo $PROJECT_FOLDER
+## log $PROJECT_FOLDER
 
 # Edit an existing cronjob:
-# crontab -l | sed '/a word or pattern that's only in the line to replace/c\* * * * * I'm the new cronjob' | crontab -
-# E.g. if you want to change the time of "30 * * * * $HOME/Stream/monitor.sh":
-# crontab -l | sed '/monitor.sh/c\15 * * * * $HOME/Stream/monitor.sh' | crontab -
+## crontab -l | sed '/a word or pattern that's only in the line to replace/c\* * * * * I'm the new cronjob' | crontab -
+# E.g. if you want to change the time of "30 * * * * $PROJECT_FOLDER/monitor.sh":
+## crontab -l | sed '/monitor.sh/c\15 * * * * $PROJECT_FOLDER/monitor.sh' | crontab -
 
 # Copy the entire content of crontab to a file called blah.txt in the home folder
-# crontab -l | cat >> blah.txt
+## crontab -l | cat >> blah.txt
 # Copy the content of crontab to the project´s log file
-# crontab -l | cat >> "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/sys.log
+## crontab -l | cat >> "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/sys.log
 # OR you can source the project functions and global vars again in commands-tray.sh
-# source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/common.sh
-# crontab -l | cat >> "$PROJECT_FOLDER/sys.log"
+## crontab -l | cat >> "$PROJECT_FOLDER/sys.log"
 
-# Send the sys.log to the email address next time blank.sh is executed.
-# ./send-email.sh
-# Send the battery.log to the email address next time blank.sh is executed.
-# ./send-email.sh -a $PROJECT_FOLDER/battery.log -s "STREAMER: Battery Log."
+# Send the sys.log to the email address next time blank.sh is executed. 
+# When I uploaded send-email.sh remotely it needed permission first. Errors from commands-tray.sh are copied in sys.log
+## sudo chmod 755 $PROJECT_FOLDER/send-email.sh
+## $PROJECT_FOLDER/send-email.sh
+# Send the battery.log
+## $PROJECT_FOLDER/send-email.sh -a $PROJECT_FOLDER/battery.log -s "STREAMER: Battery Log."
+
+# TEST BEFORE! This should delete all recordings older than 30 days
+## find $PROJECT_FOLDER/audio -type f -mtime +30 -exec rm {} \;
+
+# This will remove the dash from the log message in the update-code.py
+## sed -i '/______________UPDATE CODE/c\log_message("UPDATE CODE")' $PROJECT_FOLDER/update-code.py
